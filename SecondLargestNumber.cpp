e4d4c12b775db3967f3c016ae52f9d4a6decaeba
Ayuti-Pardeshi <162686430+Ayuti-Pardeshi@users.noreply.github.com>
2025-05-27 23:54:10 +0530

    Create SecondLargestNumber.cpp

---
+++ SecondLargestNumber.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int print2largest(vector<int>& arr) {
+    int first = -1, second = -1;
+
+    for (int num : arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num < first && num > second) {
+            second = num;
+        }
+    }
+
+    return second;
+}
+
+int main() {
+    vector<int> arr = {12, 35, 1, 10, 34, 1};
+    cout << print2largest(arr) << endl;  // Output: 34
+    return 0;
+}
