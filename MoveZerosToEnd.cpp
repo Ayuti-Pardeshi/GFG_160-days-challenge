0e40ff513970cfa998032843059e4499b335e3f2
Ayuti-Pardeshi <162686430+Ayuti-Pardeshi@users.noreply.github.com>
2025-05-28 23:34:12 +0530

    Create MoveZerosToEnd.cpp

---
+++ MoveZerosToEnd.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    void moveZerosToEnd(vector<int> &arr) {
+        int n = arr.size();
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index++] = arr[i];
+            }
+        }
+
+        while (index < n) {
+            arr[index++] = 0;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> arr = {1, 2, 0, 4, 3, 0, 5, 0};
+    sol.moveZerosToEnd(arr);
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    // Output: 1 2 4 3 5 0 0 0
+
+    return 0;
+}
